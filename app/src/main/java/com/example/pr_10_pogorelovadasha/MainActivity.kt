74b48f936406631f0ee243ae81dd7306d20b7d1c
android studio <mobil@NAVIATK.RU>
2022-05-07 11:30:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr_10_pogorelovadasha/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pr_10_pogorelovadasha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
